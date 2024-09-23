035b2d871aecb64d9377afaa89b18e29f8c86a2a
GannaGavrylova <gannadeutsch@gmail.com>
2024-09-23 19:33:03 +0200

    16_material_ui_dialog_modal_window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography align="left" variant="h6">
+            My Material UI App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" sx={{ padding: "30px" }}>
+        <Typography variant="h5" gutterBottom>
+          Добро пожаловать в наше приложение!
+        </Typography>
+        <Button color="primary" variant="contained" onClick={handleClickOpen}>
+          Открыть диалоговое окно
+        </Button>
+      </Container>
+
+      <Dialog open={isOpen} onClose={handleClose}>
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Использовать Material UI?
+        </DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>
+            Это простое React приложение с использованием Material UI. Вы можете
+            настроить его по своему усмотрению.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Отмена
+          </Button>
+          <Button autoFocus onClick={handleClose}>
+            Согласие
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
